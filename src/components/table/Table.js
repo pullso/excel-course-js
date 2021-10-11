d62d48e975420a24824da48b58d2b0c0de38b457
pullso <pullso.code@gmail.com>
2021-10-11 13:40:43 +0300

    add classes structure

---
+++ src/components/table/Table.js
@@ -0,0 +1,22 @@
+import {ExcelComponent} from '@/core/ExcelComponent';
+
+export class Table extends ExcelComponent {
+  static className = 'excel__table'
+  toHTML() {
+    return `
+      <div class="row">
+        <div class="row-info"></div>
+        <div class="row-data">
+          <div class="column">
+            A
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="row-info">1</div>
+        <div class="row-data">
+          <div class="cell selected" contenteditable="true"></div>
+        </div>
+      </div>`
+  }
+}
