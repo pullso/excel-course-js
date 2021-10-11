b27a8f269e6ae49e5406c9785dc80cd99113316a
pullso <pullso.code@gmail.com>
2021-10-11 14:13:08 +0300

    add methods in library for dom

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -2,7 +2,7 @@ import {$} from '@core/dom';
 
 export class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector)
+    this.$el = $(selector)
     this.components = options.components || []
   }
 
@@ -11,12 +11,14 @@ export class Excel {
 
     this.components.forEach(Component=>{
       const $el = $.create('div', Component.className)
-      const component = new Component($el)
-      $el.innerHTML = component.toHTML()
+      const component = new Component($el);
+      $el.html(component.toHTML())
       $root.append($el)
     })
+
     return $root
   }
+
 
   render() {
     this.$el.append(this.getRoot())
