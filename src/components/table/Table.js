654b3b8a3069cdd230e0694f7fd163ed3cfa637e
pullso <pullso.code@gmail.com>
2021-10-13 14:25:48 +0300

    add create table API

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,22 +1,9 @@
 import {ExcelComponent} from '@/core/ExcelComponent';
+import {createTable} from '@/components/table/tableTemplate';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
   toHTML() {
-    return `
-      <div class="row">
-        <div class="row-info"></div>
-        <div class="row-data">
-          <div class="column">
-            A
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="row-info">1</div>
-        <div class="row-data">
-          <div class="cell selected" contenteditable="true"></div>
-        </div>
-      </div>`
+    return createTable()
   }
 }
