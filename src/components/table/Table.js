032af0040f83f788f96901e60e59fd87c60dd88c
pullso <pullso.code@gmail.com>
2021-11-25 14:04:02 +0300

    add formula works

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -6,6 +6,7 @@ import {TableSelection} from './TableSelection';
 import {$} from '@/core/dom';
 import * as actions from '@/store/actions';
 import {defaultStyles} from '../../constants';
+import parse from '../../core/parse';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
@@ -73,9 +74,11 @@ export class Table extends ExcelComponent {
     super.init()
     this.selectCell(this.$root.find('[data-id="0:0"]'))
 
-    this.$on('formula:input', text => {
-      this.selection.current.text(text)
-      this.updateTextInStore(text)
+    this.$on('formula:input', value => {
+      this.selection.current
+          .attr('data-value', value)
+          .text(parse(value))
+      this.updateTextInStore(value)
     })
 
     this.$on('formula:done', () => {
@@ -89,11 +92,6 @@ export class Table extends ExcelComponent {
         ids: this.selection.selectedIds,
       }))
     })
-  }
-
-  onMousemove() {
-    // TODO remove console
-    console.log(`: mousemove`)
   }
 
   toHTML() {
