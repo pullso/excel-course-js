435f1b233f766033371375b5a9e716126a2aa84d
pullso <pullso.code@gmail.com>
2021-12-04 16:34:53 +0300

    add jest and test for store

--- src/pages/ExcelPage.js
+++ src/pages/ExcelPage.js
@@ -1,6 +1,6 @@
 import {Page} from '../core/Page';
 import {debounce, storage} from '../core/utils';
-import {createStore} from '../core/createStore';
+import {createStore} from '../core/store/createStore';
 import {rootReducer} from '../store/rootReducer';
 import {normalizeInitialState} from '../store/initialState';
 import {Excel} from '../components/excel/Excel';
